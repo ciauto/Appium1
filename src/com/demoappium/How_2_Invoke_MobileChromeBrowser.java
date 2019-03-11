b2f45f1a7e9663ed3b4e3a015a1d59592cc823e5
ciauto <naresh.busyqa@gmail.com>
2019-03-11 15:00:57 -0400

    added project

---
+++ src/com/demoappium/How_2_Invoke_MobileChromeBrowser.java
@@ -0,0 +1,28 @@
+package com.demoappium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
+public class How_2_Invoke_MobileChromeBrowser {
+
+	public static void main(String[] args) throws MalformedURLException {
+
+		DesiredCapabilities cap = new DesiredCapabilities();
+
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "GalaxyNexus");
+		// cap.setCapability(MobileCapabilityType.p, "Android Emulator");
+		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(url, cap);
+		String url1 = "http://www.facebook.com";
+		driver.get(url1);
+	}
+}
